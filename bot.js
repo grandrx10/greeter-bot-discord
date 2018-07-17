fc7bcf613a7d4837434fb63940927a80ccc7b133
grandrx10 <41299974+grandrx10@users.noreply.github.com>
2018-07-17 12:13:58 -0400

    added weapon functionality

--- bot.js
+++ bot.js
@@ -12,7 +12,12 @@ var bot = new Discord.Client({
 });
 
 function magic8(){
-   var magicMessages = ["Yes","No","Maybe","In your dreams.","Yeah right.","Maybe when Minehero can fly.","Go ask the real 8Ball.","Why would you ask me that. That sounds like a Phil Question.","YES! OH MY GOD WHY ARE YOU THIS DUMB!?","No, god no. Never in the history of the Earth, not even when Incomp can get decent one taps, will that be true.","I know you know the answer already.","That's funny to you isn't it?","I don't want to answer anymore of your gay ass questions.","Sure, if you think about it that way.","Yes, sadly, it is true. But I'd rather believe that it is not.","Well, yeah, if you wanna get raped.","Maybe when Kurx sounds like a man."];
+   var magicMessages = ["Yes","No","Maybe","In your dreams.","Yeah right.","Maybe when Minehero can fly.","Go ask the real 8Ball.","Why would you ask me that. That sounds like a Phil Question.","YES! OH MY GOD WHY ARE YOU THIS DUMB!?","No, god no. Never in the history of the Earth, not even when Incomp can get decent one taps, will that be true.","I know you know the answer already.","That's funny to you isn't it?","I don't want to answer anymore of your gay ass questions.","Sure, if you think about it that way.","Yes, sadly, it is true. But I'd rather believe that it is not.","Well, yeah, if you wanna get raped.","Maybe when Kurx sounds like a man.",
+   "Yes, that is indeed true.",
+   "By the asses of Frisk, that is so false.",
+   "Why would you even ask that question- I'm not a wizard you know.",
+   "This does not compute. I have no idea!",
+   "Nien, Shviendhund."];
    var randNum = Math.floor(Math.random() * Math.floor(magicMessages.length));
    return magicMessages[randNum];
 }
@@ -31,12 +36,14 @@ function joke(){
 }
 
 function love(){
-   var loveMeter = ["Yeah, it's a good match.","Gods no.","Why in the world would you want to- Nevermind.","GAY!","oooOOooo yeah, good match","You and... No, that's disgusting.","...","I didn't even think about that! That's a good idea!","Sometimes, I just want to end it all when you idoits ask me questions like those. No, its a trash idea.","Are you serious right now.","Yeah! That relationship is gay but perfect!","Wowza! You're really into it! It's a great match!"]
+   var loveMeter = ["Yeah, it's a good match.","Gods no.","Why in the world would you want to- Nevermind.","GAY!","oooOOooo yeah, good match","You and... No, that's disgusting.","...","I didn't even think about that! That's a good idea!","Sometimes, I just want to end it all when you idoits ask me questions like those. No, its a trash idea.","Are you serious right now.","Yeah! That relationship is gay but perfect!","Wowza! You're really into it! It's a great match!", "Let me ask you a question instead: Do Lady Ga Ga and Justin Bieber make a good match?",
+   "You wish it would work, but no, it doesnt."]
    var randNum = Math.floor(Math.random() * Math.floor(loveMeter.length));
    return loveMeter[randNum];
 }
 
-//THIS IS THE DATABASE (UNFORTUNATELY)
+//DATABASE ACCESS CODE
+
 const fs = require('fs');
 var txtFile = "characters.txt";
 var database1 = fs.readFileSync(txtFile,'utf8');
@@ -45,14 +52,24 @@ database = JSON.parse(database1);
 function createNewChar(username){
   const fs = require('fs');
   //"TheLastHero":{"Stats":{"HP":20,"MP":10},"Weapons":"None"}
-  var replacement = database1.slice(0,-1) + ",\"" + username + "\":{\"Stats\":{\"HP\":20,\"MP\":10},\"Weapons\":\"None\"}}"
+  var replacement = database1.substring(0, database1.length - 1) + ",\"" + username + "\":{\"Stats\":{\"HP\":20,\"MP\":10},\"Weapons\":\"None\"}}"
   fs.writeFile('characters.txt', replacement, function (err) {
     if (err) throw err;
   });
   return "Your character has been created!"
 }
 
-//DATABASE ENDS HERE
+function addWeapon(username) {
+  var weapons = ["dagger","sword","pepperspray"];
+  var randNum = Math.floor(Math.random() * Math.floor(weapons.length));
+  database[username]["Weapons"] = weapons[randNum];
+  fs.writeFile('characters.txt',JSON.stringify(database), function(err){
+    if (err) throw err;
+  });
+  return username + " has obtained the sacred " + weapons[randNum];
+}
+
+//DATABASE CODE ENDS HERE
 
 bot.on('ready', function (evt) {
     logger.info('Connected');
@@ -110,17 +127,23 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Here are the commands: \n !help \n !hi \n !magic8 (insert yes or no question) \n !love (insert name) \n !random (insert number) \n !joke'
                 });
+                break;
             case 'characters':
                 bot.sendMessage({
                     to: channelID,
-                    message: user + " has " + database[user]["Stats"]["HP"] + " hp right now. Want to smack him?"
+                    message: user + " has " + database[user]["Stats"]["HP"] + " hp right now. " + user + " has a " + database[user]["Weapons"] + "."
                 });
+                break;
             case 'start':
                 bot.sendMessage({
                     to: channelID,
                     message: createNewChar(user)
                 });
-
+            case 'scavenge':
+                bot.sendMessage({
+                    to: channelID,
+                    message: addWeapon(user)
+                });
 
             break;
             // Just add any case commands if you want to..
